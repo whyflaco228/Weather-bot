70fb7ca773462fe736248e9f1a0f2e979168399f
whyflaco228 <whyflaco228@gmail.com>
2022-08-19 13:44:59 +0600

    First commit

--- main_tg_bot.py
+++ main_tg_bot.py
@@ -5,13 +5,35 @@ from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
-bot = Bot(TG_BOT_TOKEN)
+bot = Bot(token=TG_BOT_TOKEN)
 dp = Dispatcher(bot)
 
 
-@dp.message_handlers(commands=['start'])
+@dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
     await message.reply('Привет, напиши мне название города и я пришлю сводку погоды!')
+
+
+@dp.message_handler()
+async def get_weather(message: types.Message):
+    try:
+        r = requests.get(
+            f'https://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={open_weather_token}&units=metric'
+        )
+        data = r.json()
+        city = data['name']
+        current_weather = data['main']['temp']
+        humidity = data['main']['humidity']
+        pressure = data['main']['pressure']
+        wind_speed = data['wind']['speed']
+
+        await message.reply(f'Температура в городе {city}: {current_weather}\n'
+                            f'Влажность: {humidity}\n'
+                            f'Давление:{pressure}\n'
+                            f'Скорость ветра: {wind_speed}')
+    except Exception as ex:
+        await message.reply(ex)
+        await message.reply('Проверьте название города!')
 
 
 if __name__ == '__main__':
